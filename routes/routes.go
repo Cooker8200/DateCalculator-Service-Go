a54ae0abba68eeebab7c7bf3c537a3fc28d3d3bb
Matthew Cook <cooker8200@hotmail.com>
2024-02-01 22:04:23 -0600

    CORS

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,26 @@ package routes
 import (
 	aws "DateCalculator-Service-Go/data/aws"
 	mongo "DateCalculator-Service-Go/data/mongo"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+    AllowOrigins:     []string{"http://localhost:4200", "http://localhost:3000"},
+    AllowMethods:     []string{"GET", "PUT", "DELETE", "OPTIONS"},
+    AllowHeaders:     []string{"Origin", "Access-Control-Allow-Origin"},
+    ExposeHeaders:    []string{"Content-Length"},
+    AllowCredentials: true,
+    AllowOriginFunc: func(origin string) bool {
+      return origin == "http://localhost:4200"
+    },
+    MaxAge: 12 * time.Hour,
+  }))
 
 	router.GET("/dates/aws", aws.GetAllDates)
 	router.PUT("/dates/aws", aws.AddNewDate)
