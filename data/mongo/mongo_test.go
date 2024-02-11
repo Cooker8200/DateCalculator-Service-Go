2617640c7b99fc6427b6e207de9c67f8dcb18d8c
Matthew Cook <cooker8200@hotmail.com>
2024-02-10 22:36:14 -0600

    wrote test shells, need to mock mongo and learn reading response body

--- data/mongo/mongo_test.go
+++ data/mongo/mongo_test.go
@@ -15,15 +15,62 @@ func TestTestFunc(t *testing.T) {
 		t.Errorf("got %q, wanted %q", result, expected)
 	}
  }
-
- func TestGetAllDates(t *testing.T) {
-	// TODO: godotenv mocking, mongo mocking
+ 
+ func setupHttpTests() (*httptest.ResponseRecorder, *gin.Context) {
 	recorder := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(recorder)
 
+	return recorder, c
+ }
+ 
+ // TODO: godotenv mocking, mongo mocking
+
+ func TestGetAllDates(t *testing.T) {
+	recorder, c := setupHttpTests()
+
+	// expectedBody := []primitive.M{"name": "Matt","date": "June 18","type": "Birthday"}
+
 	GetAllDates(c)
 
-	if recorder.Code != 200 {
+	response := recorder.Result()
+
+	if response.StatusCode != 200 {
+		t.Error(recorder.Code, "Failed test")
+	}
+ }
+
+ func TestAddNewDate(t *testing.T) {
+	recorder, c := setupHttpTests()
+
+	AddNewDate(c)
+
+	response := recorder.Result()
+
+	if response.StatusCode != 200 {
+		t.Error(recorder.Code, "Failed test")
+	}
+ }
+
+ func TestRemoveDate(t *testing.T) {
+	recorder, c := setupHttpTests()
+
+	RemoveDate(c)
+
+	response := recorder.Result()
+
+	if response.StatusCode != 200 {
+		t.Error(recorder.Code, "Failed test")
+	}
+ }
+
+ func TestWipeDatabase (t *testing.T) {
+	recorder, c := setupHttpTests()
+
+	WipeDatabase(c)
+
+	response := recorder.Result()
+
+	if response.StatusCode != 200 {
 		t.Error(recorder.Code, "Failed test")
 	}
  }
